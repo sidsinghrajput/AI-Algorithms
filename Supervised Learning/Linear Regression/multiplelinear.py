4f3285d51a70ee086dd800a3667d827eb33a2650
Siddharth Singh <55332990+sidsinghrajput@users.noreply.github.com>
2020-09-21 20:21:57 +0530

    Update multiplelinear.py

--- Supervised Learning/Linear Regression/multiplelinear.py
+++ Supervised Learning/Linear Regression/multiplelinear.py
@@ -1,52 +1,19 @@
 import matplotlib.pyplot as plt 
 import numpy as np 
 from sklearn import datasets, linear_model, metrics 
-  
-# load the boston dataset 
-boston = datasets.load_boston(return_X_y=False) 
-  
-# defining feature matrix(X) and response vector(y) 
+boston = datasets.load_boston(return_X_y=False)
 X = boston.data 
-y = boston.target 
-  
-# splitting X and y into training and testing sets 
+y = boston.target
 from sklearn.model_selection import train_test_split 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, 
-                                                    random_state=1) 
-  
-# create linear regression object 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=1)
 reg = linear_model.LinearRegression() 
-  
-# train the model using the training sets 
 reg.fit(X_train, y_train) 
-  
-# regression coefficients 
 print('Coefficients: \n', reg.coef_) 
-  
-# variance score: 1 means perfect prediction 
 print('Variance score: {}'.format(reg.score(X_test, y_test))) 
-  
-# plot for residual error 
-  
-## setting plot style 
 plt.style.use('fivethirtyeight') 
-  
-## plotting residual errors in training data 
-plt.scatter(reg.predict(X_train), reg.predict(X_train) - y_train, 
-            color = "green", s = 10, label = 'Train data') 
-  
-## plotting residual errors in test data 
-plt.scatter(reg.predict(X_test), reg.predict(X_test) - y_test, 
-            color = "blue", s = 10, label = 'Test data') 
-  
-## plotting line for zero residual error 
+plt.scatter(reg.predict(X_train), reg.predict(X_train) - y_train, color = "green", s = 10, label = 'Train data') 
+plt.scatter(reg.predict(X_test), reg.predict(X_test) - y_test, color = "blue", s = 10, label = 'Test data') 
 plt.hlines(y = 0, xmin = 0, xmax = 50, linewidth = 2) 
-  
-## plotting legend 
 plt.legend(loc = 'upper right') 
-  
-## plot title 
 plt.title("Residual errors") 
-  
-## function to show plot 
 plt.show() 
